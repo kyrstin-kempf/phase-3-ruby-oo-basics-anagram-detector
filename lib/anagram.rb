80346ce3e2cc86a542612862006ae3ee66321232
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2022-06-14 14:07:15 -0400

    commit tree 0e3241179277f5139233c4cc3f8ab77c8b4734a6

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+    attr_reader :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def matches(array_of_words)
+        array_of_words.chars == 
+    end
+
+end
